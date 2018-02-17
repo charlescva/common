df5f53117d4a58774c8b53e095d4a64324690ce6
Charles Ott <charles.ott@leidos.com>
2018-02-17 13:22:50 -0500

    added checks to eigen

--- conan/eigen/conanfile.py
+++ conan/eigen/conanfile.py
@@ -1,5 +1,11 @@
 from conans import ConanFile, CMake, tools
 
+'''
+ Eigen consists only of header files, hence there is nothing to compile
+before you can use it. Moreover, these header files do not depend on your
+platform, they are the same for everybody.
+'''
+
 
 class EigenConan(ConanFile):
     name = "eigen"
@@ -7,20 +13,24 @@ class EigenConan(ConanFile):
     license = "MPL-2"
     url = "http://eigen.tuxfamily.org"
     description = "A C++ template library for linear algebra: matrices, vectors, numerical solvers and related algorithms."
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "-DCMAKE_INSTALL_PREFIX": "`pwd`/packges"}
+    default_options = "shared=False"
+    generators = "cmake"
+
 
     def source(self):
         self.run("wget http://bitbucket.org/eigen/eigen/get/3.3.4.tar.gz")
         self.run("tar -xf *.tar.gz")
 
     def build(self):
-        self.run("echo Nothing to do")
+        cmake = CMake(self)
+	build_dir = "`pwd`/build"
+	cmake.configure(source_folder="eigen-eigen-5a0156e40feb", defs=self.options)
+	self.run("make -j8 check")
 
     def package(self):
-        self.copy("*hello.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+	self.run("echo Prefix targetted conan package path.")
 
     def package_info(self):
-        self.cpp_info.libs = ["hello"]
+        self.cpp_info.libs = ["eigen"]
